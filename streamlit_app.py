a9e04980605822d670b24d007bbb8f77421c8045
radni14 <rodney.s.cahalne@accenture.com>
2024-10-21 11:50:06 +0800

    Update streamlit_app.py
    added json in the request import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+ k# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -43,4 +43,4 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
